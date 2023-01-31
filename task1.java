aca58b7cd9133330d349da63131484c07d90768c
timmashkov <106866033+timmashkov@users.noreply.github.com>
2023-01-31 08:56:53 +0300

    Update task1.java

--- task1.java
+++ task1.java
@@ -1,60 +1,28 @@
-/**
- * Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
- */
-package HW2;
+//1. Пусть дан произвольный список целых чисел, удалить из него четные числа
 
-import java.io.IOException;
-import java.util.Arrays;
+package ru.geekbrains;
+
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.logging.FileHandler;
-import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
-public class task2_2 {
-
-    public static int[] randomArr() {
-        Random rand = new Random();
-        int arr[] = new int[10];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = rand.nextInt(100);
-            System.out.print(arr[i] + " ");
+public class Task001 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        ArrayList<Integer> list1 = new ArrayList<Integer>();
+        for (int i = 0; i < 10; i++) {
+            int val = rnd.nextInt(-100, 100);
+            list1.add(val);
         }
-        System.out.println("");
-        return arr;
-    }
-
-    public static int[] babblSort(int arr[]) throws IOException {
-        Logger loger = Logger.getLogger(task2_2.class.getName());
-        FileHandler fHandler = new FileHandler("Task2_2.txt");
-        SimpleFormatter sFormatter = new SimpleFormatter();
-        fHandler.setFormatter(sFormatter);
-        loger.addHandler(fHandler);
-
-        int temp;
-        for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = 0; j < arr.length - 1; j++) {
-                if (arr[j] < arr[j + 1]) {
-                    temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
+        System.out.printf("Первоначальный список %s\n", list1);
+        int i = 0;
+        while (i < list1.size()) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
             }
-
-            loger.info(Arrays.toString(arr));
+            else {
+                i++;
+            }
         }
-        return arr;
-
+        System.out.printf("Список с удаленными четными числами %s", list1);
     }
-
-    public static void print(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-
-        print(babblSort(randomArr()));
-    }
-
 }
