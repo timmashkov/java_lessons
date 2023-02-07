77fae0f7c401b2b3bc446b8d5b2cfcb6e3ef10fd
timmashkov <106866033+timmashkov@users.noreply.github.com>
2023-02-07 12:13:48 +0300

    Create 3_task.java

---
+++ 3_task.java
@@ -0,0 +1,13 @@
+package job;
+
+//  На шахматной доске расставить 8 ферзей так, 
+// чтобы они не били друг друга.
+
+public class task4 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
